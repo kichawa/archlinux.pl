f4bbb6b3351098477851aff309a3e4d9978ae707
kichawa <boxbolky@gmail.com>
2011-10-12 00:21:51 +0200

    init

---
+++ djangobb_forum/tests/__init__.py
@@ -0,0 +1,5 @@
+from test_forum import TestForum
+from test_reputation import TestReputation
+from test_profile import TestProfile
+from test_utils import TestParsers, TestPaginators
+from test_templatetags import TestLinkTags
